fffe36a04d6cac839a1ffac59de1952b0a3e8f8e
Limin Xu <limin.xu@ericsson.com>
2020-04-01 23:14:53 +0800

    Update

---
+++ kube_retag.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+images=(
+kube-apiserver:v1.17.4
+#kube-controller-manager:v1.17.4
+#kube-scheduler:v1.17.4
+#kube-proxy:v1.17.4
+#pause:3.1
+#etcd:3.4.3-0
+#coredns:1.6.5
+)
+
+for image in ${images[@]}
+do
+  docker pull xulimin1986/mirror/$image
+  docker tag xulimin1986/mirror/$image k8s.gcr.io/$image
+done
+
