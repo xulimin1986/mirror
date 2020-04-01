45f8329d0de2b167b0ad2b07f89cdfe93e0f3534
Limin Xu <limin.xu@ericsson.com>
2020-04-01 23:52:04 +0800

    Add Dockfiles in mirror
    Add Dockfiles for following image:
      kube-controller-manager:v1.17.4
      kube-scheduler:v1.17.4
      kube-proxy:v1.17.4
      pause:3.1
      etcd:3.4.3-0
      coredns:1.6.5

--- kube_retag.sh
+++ kube_retag.sh
@@ -2,17 +2,18 @@
 
 images=(
 kube-apiserver:v1.17.4
-#kube-controller-manager:v1.17.4
-#kube-scheduler:v1.17.4
-#kube-proxy:v1.17.4
-#pause:3.1
-#etcd:3.4.3-0
-#coredns:1.6.5
+kube-controller-manager:v1.17.4
+kube-scheduler:v1.17.4
+kube-proxy:v1.17.4
+pause:3.1
+etcd:3.4.3-0
+coredns:1.6.5
 )
 
 for image in ${images[@]}
 do
-  docker pull xulimin1986/mirror/$image
-  docker tag xulimin1986/mirror/$image k8s.gcr.io/$image
+  tag=$(echo $image | awk -F":" '{print $1}')
+  docker pull xulimin1986/mirror:$tag
+  docker tag xulimin1986/mirror:$tag k8s.gcr.io/$image
 done
 
